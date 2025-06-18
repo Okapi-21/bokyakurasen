7ad2e70b162886bb964d0184b22f15745e9fcc7a
okapi-21　 <okabekakuji@outlook.jp>
2025-06-19 06:07:51 +0900

    userの新規登録機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"[]
   root 'static_pages#top'
+  resources :users, only: %i[new create]
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
 end
