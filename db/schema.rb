7e6fdfe5288b7559eeddc21b871e71b44196254f
okapi-21　 <okabekakuji@outlook.jp>
2025-06-20 08:23:47 +0900

    問題一覧画面の作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_15_120000) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_19_203116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.text "description", null: false
+    t.integer "current_count", default: 0
+    t.integer "false_count", default: 0
+    t.boolean "listed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -23,4 +35,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_15_120000) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "questions", "users"
 end
